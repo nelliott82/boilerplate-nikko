bc9f15ba4fa8562600ddd7ad5749fa83d42285fc
nikkoe82 <nikkoelliott@gmail.com>
2019-10-28 13:15:37 -0500

    Added stuff.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/whatever', require('/whatever'));
+router.use('/whatever', require('./whatever'));
 
 router.use(function (req, res, next) {
     const error = new Error('Not found.');
--- server/server.js
+++ server/server.js
@@ -5,26 +5,26 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'path to public files')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', require('../api'));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './path/to/index.html'));
-});
-
-app.use(function(err, req, res, next) {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('Hello? Anyone there?');
   console.log("That depends. Who's asking?");
-  console.log(`Oh sure, sir. My name is your server number ${port}`);
+  console.log(`Oh sure, sir. My name is server number ${port}`);
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
